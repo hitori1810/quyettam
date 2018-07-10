02a9c4f007d32917e174be263d2f2747e5bf85c3
Tung Bui <buikimtung001@gmail.com>
2018-07-10 10:38:23 +0700

    update file ignore

--- cache/modules/KBDocumentKBTags/language/en_us.lang.php
+++
@@ -1,7 +0,0 @@
-<?php
-// created: 2018-03-30 10:32:17
-$mod_strings = array (
-  'LBL_TEAM' => 'Center',
-  'LBL_TEAMS' => 'Center',
-  'LBL_TEAM_ID' => 'Center Id',
-);
--- cache/smarty/templates_c/%%3D^3D0^3D06434B%%_quickcreate.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php /* Smarty version 2.6.11, created on 2018-03-30 10:33:17
-         compiled from themes/RacerX/tpls/_quickcreate.tpl */ ?>
-
-
-
-
